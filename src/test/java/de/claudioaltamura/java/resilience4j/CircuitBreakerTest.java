1d309e20747ceef9d5178b894495ede234b14303
claudioaltamura <claudio@claudioaltamura.de>
2020-08-27 16:35:35 +0200

    chg: added custom CircuitBreakerConfig

--- src/test/java/de/claudioaltamura/java/resilience4j/CircuitBreakerTest.java
+++ src/test/java/de/claudioaltamura/java/resilience4j/CircuitBreakerTest.java
@@ -1,6 +1,9 @@
 package de.claudioaltamura.java.resilience4j;
 
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.core.SupplierUtils;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +13,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CircuitBreakerTest {
 
+    private final BackEndService backEndService = new BackEndService();
+
     @Test void recoverFromExceptionBeforeCircuitBreakerFailure() {
-        BackEndService backEndService = new BackEndService();
+
         CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("default");
 
         Supplier<String> decoratedSupplier = CircuitBreaker
@@ -23,5 +28,40 @@ class CircuitBreakerTest {
         String result = circuitBreaker.executeSupplier(supplierWithRecovery);
 
         assertThat(result).isEqualTo("Hello Recovery");
+    }
+
+
+    @Test void customCircuitBreakerConfig() {
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+                .failureRateThreshold(50)
+                .minimumNumberOfCalls(6)
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .build();
+
+        CircuitBreakerRegistry circuitBreakerRegistry =
+                CircuitBreakerRegistry.of(circuitBreakerConfig);
+
+        CircuitBreaker circuitBreakerWithCustomConfig = circuitBreakerRegistry
+                .circuitBreaker("custom", circuitBreakerConfig);
+
+        circuitBreakerWithCustomConfig.getEventPublisher().onError(event -> System.out.println(event));
+
+        Supplier<String> decoratedSupplier = CircuitBreaker
+                .decorateSupplier(circuitBreakerWithCustomConfig, backEndService::doSomething);
+
+        Supplier<String> supplierWithRecovery = SupplierUtils
+                .recover(decoratedSupplier, (exception) -> "Hello Recovery");
+
+        int i = 0;
+        try {
+            for(; i < 10; i++) {
+                circuitBreakerWithCustomConfig.executeSupplier(supplierWithRecovery);
+            }
+        } catch(CallNotPermittedException e) {
+          //nothing
+        }
+
+        assertThat(i).isEqualTo(3); // slidingWindowSize 5, 4
+
     }
 }

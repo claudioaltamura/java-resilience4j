1a9d78675aef57625cdd426a27c0843c4901a672
claudioaltamura <claudio@claudioaltamura.de>
2020-09-10 14:18:49 +0200

    chg: reformatted

--- src/main/java/de/claudioaltamura/java/resilience4j/BackEndService.java
+++ src/main/java/de/claudioaltamura/java/resilience4j/BackEndService.java
@@ -2,7 +2,10 @@ package de.claudioaltamura.java.resilience4j;
 
 public class BackEndService {
 
-    public String doSomething() {
+    public String doSomething() { throw new RuntimeException("Boom"); }
+
+    public String doSomething(String s)  {
         throw new RuntimeException("Boom");
     }
+
 }

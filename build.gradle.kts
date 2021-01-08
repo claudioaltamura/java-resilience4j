62657448e4735ec251b473662fde0037ce43e65d
claudioaltamura <claudio@claudioaltamura.de>
2021-01-08 15:18:11 +0100

    chore: upgraded libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     // Apply the java plugin to add support for Java
     java
+	id ("com.github.ben-manes.versions") version "0.36.0"
 }
 
 repositories {
@@ -8,21 +11,28 @@ repositories {
 }
 
 dependencies {
-    implementation("io.github.resilience4j:resilience4j-circuitbreaker:1.5.0")
+    implementation("io.github.resilience4j:resilience4j-circuitbreaker:1.6.1")
 
     // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
 
-    testImplementation("org.assertj:assertj-core:3.16.1")
+    testImplementation("org.assertj:assertj-core:3.18.1")
 
-    testImplementation("org.mockito:mockito-core:3.5.10")
+    testImplementation("org.mockito:mockito-core:3.6.28")
 
     // Use JUnit Jupiter Engine for testing.
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_11
+	targetCompatibility = JavaVersion.VERSION_11
 }
 
 
 val test by tasks.getting(Test::class) {
-    // Use junit platform for unit tests
-    useJUnitPlatform()
+	useJUnitPlatform()
+	testLogging {
+		events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+	}
 }

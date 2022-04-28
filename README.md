5b76e86e1ebd62278255c6b89bca36b9b1b5b6d2
claudioaltamura <claudio@claudioaltamura.de>
2022-04-28 10:46:34 +0200

    chore: housekeeping

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Build Status](https://travis-ci.org/claudioaltamura/java-resilience4j.svg?branch=master)](https://travis-ci.org/claudioaltamura/java-resilience4j)
+[![Build Status](https://travis-ci.com/claudioaltamura/java-resilience4j.svg?branch=master)](https://travis-ci.com/claudioaltamura/java-resilience4j)
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,26 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    // Apply the java plugin to add support for Java
     java
-	id ("com.github.ben-manes.versions") version "0.36.0"
+    id("com.diffplug.spotless") version "6.5.0"
+    id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
-    implementation("io.github.resilience4j:resilience4j-circuitbreaker:1.6.1")
+    implementation("io.github.resilience4j:resilience4j-circuitbreaker:1.7.1")
 
-    // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
 
-    testImplementation("org.assertj:assertj-core:3.18.1")
+    testImplementation("org.assertj:assertj-core:3.22.0")
 
-    testImplementation("org.mockito:mockito-core:3.6.28")
+    testImplementation("org.mockito:mockito-core:4.5.1")
 
-    // Use JUnit Jupiter Engine for testing.
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
-
-java {
-	sourceCompatibility = JavaVersion.VERSION_11
-	targetCompatibility = JavaVersion.VERSION_11
-}
-
 
 val test by tasks.getting(Test::class) {
 	useJUnitPlatform()
